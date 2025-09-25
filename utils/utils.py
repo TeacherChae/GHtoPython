59f6bcc6799bcb2dc8ce9b20d5f2ce70acbe32da
Keon Chae <brianckh718@gmail.com>
2025-09-25 21:57:56 +0900

    utils

---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+# ! python3
+# venv: HakLeeGHStudy
+from typing import Union, Optional
+
+def make_number(input_list:list[Optional[int]])->list[Union[int, float]]:
+    number_list = []
+    for element in input_list:
+        try:
+            number_list.append(int(element))
+        except ValueError:
+            try:
+                number_list.append(float(element))
+            except:
+                raise ValueError
+    return number_list
